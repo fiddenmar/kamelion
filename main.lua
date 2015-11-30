93e3457c35da96a2e7f6135ae4c8ad95aaa632d6
Evgeny Petrov <fiddenmar@gmail.com>
2015-11-30 18:25:14 +0300

    better physics and structure

--- main.lua
+++ main.lua
@@ -9,18 +9,16 @@ loader.path = "maps/"
 local HC = require "HardonCollider"
 
 local hero
-local hero_type
+local world = {}
 local collider
 local allSolidTiles
 local startX
 local startY
-local kamelion = false
-local adopt = false
 
 function love.load()
 	
 	-- load the level and bind to variable map
-	map = loader.load("level1.tmx")
+	map = loader.load("level3.tmx")
 	
 	-- load HardonCollider, set callback to on_collide and size of 100
 	collider = HC(1000, on_collide)
@@ -28,10 +26,9 @@ function love.load()
 	-- find all the tiles that we can collide with
 	allSolidTiles = findTiles(map)
 
+	setupWorld()
 	-- set up the hero object, set him to position 32, 32
 	setupHero(startX, startY)
-
-	hero_type = "red"
 	
 end
 
@@ -69,41 +66,54 @@ function on_collide(dt, shape_a, shape_b, mtv_x, mtv_y)
 end
 
 function collideHeroWithTile(dt, shape_a, shape_b, mtv_x, mtv_y)
-	
-	if kamelion then
-		return
-	end
+
 	-- sort out which one our hero shape is
 	if shape_a.type == "start" or shape_b.type == "start" then
 		return
 	end
 	local hero_shape, tileshape
-	if shape_a == hero and shape_b.type ~= hero_type then
+	if shape_a == hero and shape_b.type ~= hero.type then
 		hero_shape = shape_a
 		tileshape = shape_b
-	elseif shape_b == hero and shape_a.type ~= hero_type then
+	elseif shape_b == hero and shape_a.type ~= hero.type then
 		hero_shape = shape_b
 		tileshape = shape_a
 	else
 		-- none of the two shapes is a tile, return to upper function
 		return
 	end
+ 
+	if hero.kamelion then
+		if tileshape ~= "gray" then
+			return
+		end
+	end
 
-	if adopt and (math.abs(mtv_x) > 8 or math.abs(mtv_y) > 8) then
-		adopt = false
-		hero_type = tileshape.type
+	if hero.adopt and (math.abs(mtv_x) > 8 or math.abs(mtv_y) > 8) then
+		hero.adopt = false
+		hero.type = tileshape.type
 		return
 	end
 	-- why not in one function call? because we will need to differentiate between the axis later
+	hero.ySpeed = 0
 	hero_shape:move(mtv_x, 0)
 	hero_shape:move(0, mtv_y)
 	
 end
 
+function setupWorld()
+	world.gravity = 9.8
+end
+
 function setupHero(x,y)
 	
 	hero = collider:addRectangle(x,y,16,16)
-	hero.speed = 100
+	hero.xSpeedLimit = 100
+	hero.yJumpSpeed = 100
+	hero.ySpeed = 0
+	hero.type = "red"
+	hero.kamelion = false
+	hero.adopt = false
 --	hero.img = love.graphics.newImage("img/hero.png")
 
 end
@@ -111,21 +121,21 @@ end
 function handleInput(dt)
 	
 	if love.keyboard.isDown("left") then
-		hero:move(-hero.speed*dt, 0)
+		hero:move(-hero.xSpeedLimit*dt, 0)
 	end
 	if love.keyboard.isDown("right") then
-		hero:move(hero.speed*dt, 0)		
+		hero:move(hero.xSpeedLimit*dt, 0)		
 	end
 	if love.keyboard.isDown("up") then
-		hero:move(0, -(hero.speed+50)*dt)
+		hero:move(0, -(hero.xSpeedLimit+50)*dt)
 	end
-	if love.keyboard.isDown("lctrl") then
-		kamelion = true
-		adopt = false
+	if love.keyboard.isDown("lctrl", "rctrl") then
+		hero.kamelion = true
+		hero.adopt = false
 	else
-		if kamelion then
-			adopt = true
-			kamelion = false
+		if hero.kamelion then
+			hero.adopt = true
+			hero.kamelion = false
 		end
 	end
 	
@@ -134,13 +144,16 @@ end
 function updateHero(dt)
 	
 	-- apply a downward force to the hero (=gravity)
-	hero:move(0,dt*50)
-	
+	hero.ySpeed = hero.ySpeed - world.gravity * dt
+	hero:move(0, - hero.ySpeed)
+	if hero.type == "dark_gray" then
+		setupHero(startX, startY)
+	end
 end
 
 function findTiles(map)
 	local colourTiles = {}
-	local ground_types = {"red", "green", "blue", "gray", "finish", "start"} 
+	local ground_types = {"red", "green", "blue", "gray", "dark_gray", "finish", "start"} 
 
 	for i, ground_type in ipairs(ground_types) do
 		local tileGroup = {}
